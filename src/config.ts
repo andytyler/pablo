3d6cea9dc863fb8977d8cfa01359360042d78136
Andy Tyler <andrewjtyler1@gmail.com>
2025-03-28 20:25:22 -0700

    add config

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+export const site_config = {
+	site_name: 'Pablo AI',
+	site_description: 'text to design.',
+	domain: 'https://pabloai.com',
+	image: 'https://pabloai.com/og-image.png',
+	keywords: ['pablo', 'ai', 'design'],
+	socials: {
+		twitter: 'https://twitter.com/pabloai',
+		github: 'https://github.com/pabloai',
+		linkedin: 'https://linkedin.com/in/pabloai'
+	}
+};
